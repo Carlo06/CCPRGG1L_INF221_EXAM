1817d0cbae1f42ea1f6fb5c50ef2cc24f5f9e5fb
Jhon Carlo Palatino <111470308+Carlo06@users.noreply.github.com>
2022-09-05 09:42:08 +0800

    Add files via upload

---
+++ exam_1/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+       Student Me = new Student();
+       Me.firstName = "Jhon Carlo";
+       Me.middleInitial = 'R';
+       Me.Surname = "Palatino";
+       Me.dateOfBirth = 02/06/03;
+       Me.studentNumber = 2022101184;
+       Me.emailAddress = "palatinoj@students.national-u.edu.ph";
+       Me.iAmAwesome = true;
+       Me.sayMystudentNumber();
+       Me.sayMyEmailAddress();
+       Me.AmIAwesome();
+    }
+}
